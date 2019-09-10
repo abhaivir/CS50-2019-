25d2bba70db0ba4886d78e87df6f61b959621dc8
Abhaivir Singh <55149229+abhaivir@users.noreply.github.com>
2019-09-11 02:27:15 +0530

    checking conditions in c

---
+++ conditional.c
@@ -0,0 +1,15 @@
+/* checking for conditions in c*/
+# include<stdio.h>
+int main(){
+int x=10;
+int y=20;
+if(x<y){
+    printf("x is less than y");
+}
+else if(x==y){
+    printf(" equal case ");
+}
+else{
+    printf("x is greater than y");
+}
+}
